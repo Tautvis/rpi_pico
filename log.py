fff92a09323032d0a6aa5083c8ca0616d3fa8c8d
Tautvydas Misiunas <tautvydas.misiunas@gmail.com>
2022-12-15 01:48:52 +0100

    Update log functions to full scale.

--- log.py
+++ log.py
@@ -3,7 +3,7 @@
 # Where to log.
 _destination = print
 # Available log levels.
-_LOG_LEVELS = {'ALL': 40, 'DEBUG': 30, 'INFO': 20, 'WARN': 10, 'ERROR': 0}
+_LOG_LEVELS = {'ALL': 40, 'DEBUG': 30, 'INFO': 20, 'WARN': 10, 'ERROR': 0, 'OFF': -1}
 # Log level.
 _level = _LOG_LEVELS['ALL']
 
@@ -17,7 +17,18 @@ def set_level(new_level) -> None:
         _level = _LOG_LEVELS[new_level]
     else:
         print(f'log.set_level: Level must be int/str. Received: {new_level}')
-        
+
+def error(msg: str) -> None:
+    if _level >= 0:
+        _destination(msg)        
+
+def warn(msg: str) -> None:
+    if _level >= 10:
+        _destination(msg)
+
+def info(msg: str) -> None:
+    if _level >= 20:
+        _destination(msg)
         
 def debug(msg: str) -> None:
     if _level >= 30:
@@ -26,3 +37,13 @@ def debug(msg: str) -> None:
 def trace(msg: str) -> None:
     if _level >= 40:
         _destination(msg)
+        
+# Log level functions.
+LOG_FUNCTIONS = {
+    'ALL': trace,
+    'DEBUG': debug,
+    'INFO': info,
+    'WARN': warn,
+    'ERROR': error,
+    'OFF': lambda msg: None
+}
