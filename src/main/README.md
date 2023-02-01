50158a24f5c56c3c3178e23fede75d83bbd99f47
Tautvydas Misiunas <tautvydas.misiunas@gmail.com>
2023-02-02 00:06:41 +0100

    Moved git base one folder up

---
+++ src/main/README.md
@@ -0,0 +1,9 @@
+## Sensors
+
+1. Bedroom.
+2. 
+3. Kitchen. Flaky wifi? 
+4. Bathroom.
+5.
+6. CO2 sensor. Office.
+7.
---
+++ src/tests/pico_test.py
@@ -0,0 +1,6 @@
+"""Unit tests for the code. Run on Pico device.
+1. Select which tests to run.
+2. Upload libraries to Pico device.
+3. Click run this file, no need to upload tests to Pico device.
+"""
+
